80dac21b9fc2d32e929df85e0a2bd4aab7f211df
alphapk <neevarp1729@gmail.com>
2019-02-08 21:47:37 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,307 @@
+<?php include('form_process.php'); ?>
+<?php echo ""; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PM FASHIONS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+    <link href="css/creative.css" rel="stylesheet">
+    <link href="css/infinite-slider.css" rel="stylesheet">
+    <style type="text/css">
+ 
+    </style>
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">PM FASHIONS</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services">Clients</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Products</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead text-center d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <strong style="color: #f05f40">Here Definitely you will get best quality products for best price</strong>
+            </h1>
+            <hr>
+          </div>
+          <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5" style="color: #f05f40">"It's not about ideas. It's about making ideas happen."</p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">We've got what you need!</h2>
+            <hr class="light my-4">
+            <p class="text-faded mb-4">PM Fashions is now developing company in manufacturing Trolley bags, U bags, Bin, Pouches, Antistatic Uniforms etc., for automative industries. Actually PM Fashion is established in the year 1998 as manufacturer of hand quilting , patch works & some of the cotton products using hand quiltingfor designing. Which exported to Karur, Hosur and for some foreign countries like Australia, Europe etc.,. And now nearly 5 years, we are manufacturing trolley bags for automative industries at nearby places of our company. Now we are developing our company to next level.</p>
+            <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+ <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Clients</h2>
+            <hr class="my-4">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row customer-logos slider">
+           <div class="slide"><img src="img/client1.png"></div>
+    <div class="slide"><img src="img/logo - 1.jpg"></div>
+    <div class="slide"><img src="img/logo - 2.jpg"></div>
+    <div class="slide"><img src="img/logo - 3.jpg"></div>
+    <div class="slide"><img src="img/logo - 4.jpg"></div>
+    <div class="slide"><img src="img/logo - 5.gif"></div>
+    <div class="slide"><img src="img/logo - 6.png"></div>
+    <div class="slide"><img src="img/logo - 7.jpg"></div>
+     <div class="slide"><img src="img/logo - 8.png"></div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="p-0" id="portfolio">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters popup-gallery">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/t1.jpg">
+              <img class="img-t" src="img/portfolio/thumbnails/t1.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Bags
+                  </div>
+                  <div class="project-name">
+                    Trolley
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/t2.jpg">
+              <img class="img-t" src="img/portfolio/thumbnails/t2.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Bags
+                  </div>
+                  <div class="project-name">
+                    Trolley
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/t3.jpg">
+              <img class="img-t" src="img/portfolio/thumbnails/t3.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Bags
+                  </div>
+                  <div class="project-name">
+                    Trolley
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/t4.jpg">
+              <img class="img-t" src="img/portfolio/thumbnails/t4.jpeg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Bags
+                  </div>
+                  <div class="project-name">
+                    Trolley
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/t5.jpg">
+              <img class="img-t" src="img/portfolio/thumbnails/t5.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Bags
+                  </div>
+                  <div class="project-name">
+                   Trolley
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/t6.jpg">
+              <img class="img-t" src="img/portfolio/thumbnails/t6.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Bags
+                  </div>
+                  <div class="project-name">
+                   Trolley
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="bg-dark text-white">
+      <div class="container text-center">
+        <h2 class="mb-4">Always Deliver More Than Expected!</h2>
+        <a class="btn btn-light btn-xl sr-button" href="#contact">Contact Now!</a>
+      </div>
+    </section>
+
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Let's Get In Touch!</h2>
+            <hr class="my-4">
+            <p class="mb-5">Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+          </div>
+        </div>
+         
+
+<div class="contact-clean container">  
+  <form id="contact" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+    
+    <fieldset class="form-group">
+      <input class="form-control" placeholder="Name" type="text" name="name" value="<?= $name ?>" tabindex="1">
+      <span class="error"><?= $name_error ?></span>
+    </fieldset>
+    <fieldset class="form-group">
+      <input class="form-control" placeholder="Email" type="text" name="email" value="<?= $email ?>" tabindex="2">
+      <span class="error"><?= $email_error ?></span>
+    </fieldset>
+    <fieldset class="form-group">
+      <input class="form-control" placeholder="Phone Number" type="text" name="phone" value="<?= $phone ?>" tabindex="3">
+      <span class="error"><?= $phone_error ?></span>
+    </fieldset>
+        <fieldset class="form-group">
+      <textarea class="form-control" placeholder="Message" value="<?= $message ?>" name="message" tabindex="5">
+      </textarea>
+    </fieldset>
+    <fieldset class="form-group text-center">
+      <button class="btn btn-primary" name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+    <div class="success"><?= $success ?></div>
+  </form>
+</div>
+  <div class="row">
+          <div class="col-lg-3 ml-auto text-center">
+            <i class="fas fa-phone fa-3x mb-3 sr-contact-1"></i>
+            <p>9080164807</p>
+          </div>
+          <div class="col-lg-3 mr-auto text-center">
+            <i class="fas fa-envelope fa-3x mb-3 sr-contact-2"></i>
+            <p>
+              <a href="mailto:pmfashion@yahoo.co.in">pmfashion@yahoo.co.in</a>
+            </p>
+        </div>
+        <div class="col-lg-3 ml-auto text-center">
+            <i class="fas fa-address-card fa-3x mb-3 sr-contact-1"></i>
+            <p>No: 5, Kavi Kambar Street
+                Sekizhar nagar,
+                Kundrathur, Chennai - 600069</p>
+          </div>
+          <div class="col-lg-3 ml-auto text-center">
+            <i class=" fa fa-map-marker  fa-3x mb-3 sr-contact-1 aria-hidden="true""></i>
+            <a href="https://goo.gl/maps/QaWuj725Pe32"><p>Locate us in Google Maps</p></a>
+          </div>
+      </div>
+ </div>
+
+    </section>
+   
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+    <script src="js/carousel.js"></script>
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+
+  </body>
+
+</html>
